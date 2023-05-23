61a8b165a2ce44614316be85dd101d3144b7c2e1
Matpdev <matheus2ep@gmail.com>
2023-05-22 21:15:44 -0300

    FIX: Fixed links

--- src/components/parts/links/linksComponent.jsx
+++ src/components/parts/links/linksComponent.jsx
@@ -13,13 +13,25 @@ export default function Links() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
       >
-        <a href="http://" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.instagram.com/matheus_alvesdg/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FiInstagram size={30}></FiInstagram>
         </a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
+        <a
+          href="http://github.com/matpdev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FiGithub size={30}></FiGithub>
         </a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.linkedin.com/in/matheus-alves-dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FiLinkedin size={30}></FiLinkedin>
         </a>
         <div className={styles.verticalLine}></div>
